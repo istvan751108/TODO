af4fb06f0911e26b850ad9ba1a074a56a3d1ebdd
István Szabó <istvan751108@gmail.com>
2023-11-16 18:00:26 +0100

    Modified install.bat and deleted install.ps1

--- install.bat
+++ install.bat
@@ -5,12 +5,23 @@ pushd server
 call composer install
 popd
 
+:: Létrehozza a start.bat fájlt
+echo @echo off > start.bat
+echo cd server >> start.bat
+echo start /B php artisan serve >> start.bat
+echo cd ../client/app >> start.bat
+echo start /B npm run dev >> start.bat
+echo start http://localhost:5173/ >> start.bat
+
 :: Telepíti a React/Vite függőségeket
 cd client
 cd app
 npm install bootstrap-icons react-icons --save react-modal react-bootstrap@next
 
-:: Visszatér a kezdőkönyvtárba
-cd ../..
+
+
+
+
+
 
 
